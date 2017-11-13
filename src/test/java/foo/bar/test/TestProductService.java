38d9fedfc2357b0beaae1821e22079751be9210a
jesuslopezpino <jesus_lopez_pino@hotmail.com>
2017-11-13 22:47:27 +0100

    examples

--- src/test/java/foo/bar/test/TestProductService.java
+++ src/test/java/foo/bar/test/TestProductService.java
@@ -1,28 +1,30 @@
 package foo.bar.test;
 
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 import foo.bar.domain.Product;
 import foo.bar.service.impl.ProductServiceImpl;
+import foo.bar.service.utils.HqlConditions;
 
-public class TestProductService extends TestCommon<ProductServiceImpl, Product>{
-
-	@Override
-	public void testFindByExample() {
-		// TODO Auto-generated method stub
-	}
+public class TestProductService extends TestCommon<ProductServiceImpl, Product> {
 
 	@Override
 	protected Map<String, String> initFilter() {
-		// TODO Auto-generated method stub
-		return null;
+		Map<String, String> filter = new HashMap<String, String>();
+		filter.put(Product.DESCRIPTION, HqlConditions.LIKE_IGNORE_CASE);
+		filter.put(Product.NAME, HqlConditions.LIKE_IGNORE_CASE);
+		return filter;
 	}
 
 	@Override
 	protected Product[] initExamples() {
-		// TODO Auto-generated method stub
-		return null;
+		Product example1 = new Product();
+		example1.setName("Apple");
+		Product example2 = new Product();
+		example2.setDescription("fruit");
+		Product[] examples = { example1, example2 };
+		return examples;
 	}
 
 }
