358d4e599bdbb9bc4074fa6ae3d50e0617e10a38
jesuslopezpino <jesus_lopez_pino@hotmail.com>
2017-11-13 20:59:14 +0100

    New cases to consider

--- src/main/java/foo/bar/service/utils/HqlConditions.java
+++ src/main/java/foo/bar/service/utils/HqlConditions.java
@@ -8,4 +8,18 @@ public class HqlConditions {
 	public static final String NOT_EQUALS = " <> ";
 	public static final String BETWEEN = " between ";
 	public static final String IN = " in ";
+
+	// TODO: posible cases to configure
+	public static final String GREATER_THAN = " > ";
+	public static final String GREATER_EQUALS = " >= ";
+	public static final String LOWER_THAN = " < ";
+	public static final String LOWER_EQUALS = " <= ";
+	public static final String NOT_IN = " not in ";
+	public static final String IS_NOT_NULL = " is null ";
+	public static final String IS_EMPTY = " is empty ";
+	public static final String IS_NOT_EMPTY = " is not empty ";
+	public static final String MEMBER_OF = " member of ";
+	public static final String NOT_MEMBER_OF = " not member of ";
+	
+	
 }
