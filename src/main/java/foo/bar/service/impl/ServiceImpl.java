45711c2465e629acb6b93474c4343b93fa46a939
jesuslopezpino <jesus_lopez_pino@hotmail.com>
2017-11-17 03:11:47 +0100

    service refactor

--- src/main/java/foo/bar/service/impl/ServiceImpl.java
+++ src/main/java/foo/bar/service/impl/ServiceImpl.java
@@ -12,15 +12,16 @@ import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 import org.apache.log4j.Logger;
+
 import foo.bar.domain.BasicVO;
 import foo.bar.exceptions.ExampleQueryException;
 import foo.bar.exceptions.UniqueException;
 import foo.bar.service.Service;
 import foo.bar.service.utils.HqlConditions;
+import foo.bar.service.utils.UtilsService;
 import foo.bar.utils.Utils;
 
 public abstract class ServiceImpl<VO extends BasicVO<?>> implements Service<VO> {
-
 
 	EntityManager entityManager;
 
@@ -102,40 +103,6 @@ public abstract class ServiceImpl<VO extends BasicVO<?>> implements Service<VO> 
 		this.entityManager = entityManager;
 	}
 
-	private String getClauseIsNullOrNotNull(String tableName, String filterField, HqlConditions condition) {
-		return " and (" + tableName + "." + filterField + " " + condition + ")";
-	}
-
-	private String getClauseLike(String tableName, String filterField, final HqlConditions condition,
-			String nameForParameter) {
-		return " and (" + tableName + "." + filterField + " " + condition + ":" + nameForParameter + ")";
-	}
-
-	private String getClauseLikeIgnoreCase(String tableName, String filterField, final HqlConditions condition,
-			String nameForParameter) {
-		return " and (UPPER(" + tableName + "." + filterField + ")" + condition + ":" + nameForParameter + ")";
-	}
-
-	private String getClauseConditionCase(String tableName, String filterField, final HqlConditions condition,
-			String nameForParameter) {
-		return " and (" + tableName + "." + filterField + "" + condition + ":" + nameForParameter + ")";
-	}
-
-	private String getNameForParameter(String filterField, HqlConditions condition) {
-		String result = null;
-		switch (condition) {
-		case IS_NOT_EMPTY:
-		case IS_NOT_NULL:
-		case IS_EMPTY:
-		case IS_NULL:
-			break;
-		default:
-			result = filterField.replaceAll("\\.", "_");
-			break;
-		}
-		return result;
-	}
-
 	private String createCustomSelect(String[] fields) {
 		// String select = "select new " + voClass.getName() + " ( ";
 		// int fieldsLength = fields.length;
@@ -168,6 +135,7 @@ public abstract class ServiceImpl<VO extends BasicVO<?>> implements Service<VO> 
 
 	private Query createQueryForExample(VO example, Map<String, HqlConditions> filter, String select)
 			throws ExampleQueryException {
+
 		Map<String, Object> parameters = new HashMap<>();
 		String from = " from " + voClass.getName() + " tabla";
 		String where = " where 1=1";
@@ -175,28 +143,14 @@ public abstract class ServiceImpl<VO extends BasicVO<?>> implements Service<VO> 
 			Entry<String, HqlConditions> type = iterator.next();
 			HqlConditions condition = type.getValue();
 			String filterField = type.getKey();
-			String fieldForQuery = null;
-			Object valueForQuery = null;
-
-			// first check if the field is transient or not
-			boolean isTransient = Utils.isTransientField(filterField, example);
-			if (isTransient) {
-				// field for query = the referenced field
-				fieldForQuery = Utils.getReferencedField(example, filterField);
-			} else {
-				// field for query = the current field name
-				fieldForQuery = filterField;
-			}
-			valueForQuery = Utils.getFieldValue(example, filterField);
-
-			// then check if we have to apply the current value to the query
-			// (depends on condition + value)
-			boolean applyValue = this.hasToApplyConditionForQuery(condition, valueForQuery);
+			String fieldForQuery = UtilsService.getFieldForQuery(example, filterField);
+			Object valueForQuery = Utils.getFieldValue(example, filterField);
+			boolean applyValue = UtilsService.hasToApplyConditionForQuery(condition, valueForQuery);
 			if (applyValue) {
-				String nameForParameter = getNameForParameter(filterField, condition);
-				where += getClauseCondition("tabla", fieldForQuery, condition, nameForParameter);
+				String nameForParameter = UtilsService.getNameForParameter(filterField, condition);
+				where += UtilsService.getClauseCondition("tabla", fieldForQuery, condition, nameForParameter);
 				if (nameForParameter != null) {
-					Object fixedValueForQuery = this.fixValueForQuery(valueForQuery, condition);
+					Object fixedValueForQuery = UtilsService.fixValueForQuery(valueForQuery, condition);
 					parameters.put(nameForParameter, fixedValueForQuery);
 				}
 			}
@@ -212,94 +166,6 @@ public abstract class ServiceImpl<VO extends BasicVO<?>> implements Service<VO> 
 			query.setParameter(parameterKey, parameterValue);
 		}
 		return query;
-	}
-
-	private Object fixValueForQuery(Object valueForQuery, HqlConditions condition) {
-		Object result = null;
-		String stringValue = null;
-		switch (condition) {
-		case LIKE:
-			stringValue = (String) valueForQuery;
-			result = "%" + stringValue + "%";
-			break;
-		case LIKE_IGNORE_CASE:
-			stringValue = (String) valueForQuery;
-			result = "%" + stringValue.toUpperCase() + "%";
-			break;
-		default:
-			// TODO: consider date formats here!!!
-			result = valueForQuery;
-			break;
-		}
-		return result;
-	}
-
-	private String getClauseCondition(String tableName, String filterField, HqlConditions condition, String nameForParameter) {
-		String result = null;
-		switch (condition) {
-		case LIKE:
-			result = getClauseLike("tabla", filterField, condition, nameForParameter);
-			break;
-		case LIKE_IGNORE_CASE:
-			result = getClauseLikeIgnoreCase("tabla", filterField, condition, nameForParameter);
-			break;
-		case IS_NOT_EMPTY:
-		case IS_NOT_NULL:
-		case IS_EMPTY:
-		case IS_NULL:
-			result = getClauseIsNullOrNotNull("tabla", filterField, condition);
-			break;
-		// case HqlConditions.BETWEEN:
-		case EQUALS:
-		case GREATER_EQUALS:
-		case GREATER_THAN:
-		case IN:
-		case LOWER_EQUALS:
-		case LOWER_THAN:
-		case NOT_EQUALS:
-		case NOT_IN:
-			result = getClauseConditionCase("tabla", filterField, condition, nameForParameter);
-			break;
-		// case HqlConditions.MEMBER_OF:
-		// case HqlConditions.NOT_MEMBER_OF:
-		// // TODO: consider???
-		// break;
-		}
-		return result;
-	}
-
-	private boolean hasToApplyConditionForQuery(HqlConditions condition, Object value) {
-		boolean result = false;
-		switch (condition) {
-		case IS_NOT_EMPTY:
-		case IS_NOT_NULL:
-		case IS_EMPTY:
-		case IS_NULL:
-			result = true;
-			break;
-		// case HqlConditions.BETWEEN:
-		case EQUALS:
-		case GREATER_EQUALS:
-		case GREATER_THAN:
-		case IN:
-		case LIKE:
-		case LIKE_IGNORE_CASE:
-		case LOWER_EQUALS:
-		case LOWER_THAN:
-		case NOT_EQUALS:
-		case NOT_IN:
-			if (value != null) {
-				result = true;
-			}
-			break;
-			// TODO: consider???
-			// case HqlConditions.MEMBER_OF:
-			// case HqlConditions.NOT_MEMBER_OF:
-			// break;
-		default:
-			break;
-		}
-		return result;
 	}
 
 }
---
+++ src/main/java/foo/bar/service/utils/UtilsService.java
@@ -0,0 +1,142 @@
+package foo.bar.service.utils;
+
+import foo.bar.utils.Utils;
+
+public class UtilsService {
+
+	public static String getFieldForQuery(Object example, String filterField) {
+		String fieldForQuery;
+		// first check if the field is transient or not
+		boolean isTransient = Utils.isTransientField(filterField, example);
+		if (isTransient) {
+			// field for query = the referenced field
+			fieldForQuery = Utils.getReferencedField(example, filterField);
+		} else {
+			// field for query = the current field name
+			fieldForQuery = filterField;
+		}
+		return fieldForQuery;
+	}
+	
+	public static boolean hasToApplyConditionForQuery(HqlConditions condition, Object value) {
+		boolean result = false;
+		switch (condition) {
+		case IS_NOT_EMPTY:
+		case IS_NOT_NULL:
+		case IS_EMPTY:
+		case IS_NULL:
+			result = true;
+			break;
+		// case HqlConditions.BETWEEN:
+		case EQUALS:
+		case GREATER_EQUALS:
+		case GREATER_THAN:
+		case IN:
+		case LIKE:
+		case LIKE_IGNORE_CASE:
+		case LOWER_EQUALS:
+		case LOWER_THAN:
+		case NOT_EQUALS:
+		case NOT_IN:
+			if (value != null) {
+				result = true;
+			}
+			break;
+			// TODO: consider???
+			// case HqlConditions.MEMBER_OF:
+			// case HqlConditions.NOT_MEMBER_OF:
+			// break;
+		default:
+			break;
+		}
+		return result;
+	}
+	
+	public static String getClauseCondition(String tableName, String filterField, HqlConditions condition, String nameForParameter) {
+		String result = null;
+		switch (condition) {
+		case LIKE:
+			result = getClauseLike("tabla", filterField, condition, nameForParameter);
+			break;
+		case LIKE_IGNORE_CASE:
+			result = getClauseLikeIgnoreCase("tabla", filterField, condition, nameForParameter);
+			break;
+		case IS_NOT_EMPTY:
+		case IS_NOT_NULL:
+		case IS_EMPTY:
+		case IS_NULL:
+			result = getClauseIsNullOrNotNull("tabla", filterField, condition);
+			break;
+		// case HqlConditions.BETWEEN:
+		case EQUALS:
+		case GREATER_EQUALS:
+		case GREATER_THAN:
+		case IN:
+		case LOWER_EQUALS:
+		case LOWER_THAN:
+		case NOT_EQUALS:
+		case NOT_IN:
+			result = getClauseConditionCase("tabla", filterField, condition, nameForParameter);
+			break;
+		// case HqlConditions.MEMBER_OF:
+		// case HqlConditions.NOT_MEMBER_OF:
+		// // TODO: consider???
+		// break;
+		}
+		return result;
+	}
+	
+	public static String getClauseIsNullOrNotNull(String tableName, String filterField, HqlConditions condition) {
+		return " and (" + tableName + "." + filterField + " " + condition + ")";
+	}
+
+	public static String getClauseLike(String tableName, String filterField, final HqlConditions condition,
+			String nameForParameter) {
+		return " and (" + tableName + "." + filterField + " " + condition + ":" + nameForParameter + ")";
+	}
+
+	public static String getClauseLikeIgnoreCase(String tableName, String filterField, final HqlConditions condition,
+			String nameForParameter) {
+		return " and (UPPER(" + tableName + "." + filterField + ")" + condition + ":" + nameForParameter + ")";
+	}
+
+	private static String getClauseConditionCase(String tableName, String filterField, final HqlConditions condition,
+			String nameForParameter) {
+		return " and (" + tableName + "." + filterField + "" + condition + ":" + nameForParameter + ")";
+	}
+
+	public static String getNameForParameter(String filterField, HqlConditions condition) {
+		String result = null;
+		switch (condition) {
+		case IS_NOT_EMPTY:
+		case IS_NOT_NULL:
+		case IS_EMPTY:
+		case IS_NULL:
+			break;
+		default:
+			result = filterField.replaceAll("\\.", "_");
+			break;
+		}
+		return result;
+	}
+	
+	public static Object fixValueForQuery(Object valueForQuery, HqlConditions condition) {
+		Object result = null;
+		String stringValue = null;
+		switch (condition) {
+		case LIKE:
+			stringValue = (String) valueForQuery;
+			result = "%" + stringValue + "%";
+			break;
+		case LIKE_IGNORE_CASE:
+			stringValue = (String) valueForQuery;
+			result = "%" + stringValue.toUpperCase() + "%";
+			break;
+		default:
+			// TODO: consider date formats here!!!
+			result = valueForQuery;
+			break;
+		}
+		return result;
+	}
+}
