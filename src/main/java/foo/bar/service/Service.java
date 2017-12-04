f3c573e021575d67c756b5558485bf4b05f6afe8
jesuslopezpino <jesus_lopez_pino@hotmail.com>
2017-12-04 21:52:54 +0100

    exceptions

--- src/main/java/foo/bar/service/Service.java
+++ src/main/java/foo/bar/service/Service.java
@@ -1,5 +1,6 @@
 package foo.bar.service;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Map;
 
@@ -13,11 +14,13 @@ public interface Service<VO extends BasicVO<?>> {
 	public VO findByPk(Object primaryKey);
 
 	public VO findCustomByPk(Object primaryKey, String[] fields);
-	
+
 	public List<VO> findByExample(VO example, Map<String, HqlConditions> filter) throws ExampleQueryException;
 
-	public List<VO> findCustomByExample(VO example, String[] fields, Map<String, HqlConditions> filter) throws ExampleQueryException;
-	
+	public List<VO> findCustomByExample(VO example, String[] fields, Map<String, HqlConditions> filter)
+			throws ExampleQueryException, NoSuchMethodException, SecurityException, InstantiationException,
+			IllegalAccessException, IllegalArgumentException, InvocationTargetException;
+
 	boolean delete(VO element);
 
 	public VO save(VO element) throws UniqueException;
