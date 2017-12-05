715bf09b34d8af3444a89e82507e10031c8e40d8
jesuslopezpino <jesus_lopez_pino@hotmail.com>
2017-12-05 19:58:03 +0100

    covering hqlconditions

--- src/test/java/foo/bar/test/TestCustomerService.java
+++ src/test/java/foo/bar/test/TestCustomerService.java
@@ -41,13 +41,13 @@ public class TestCustomerService extends TestCommon<CustomerServiceImpl, Custome
 		filter.put(Customer.BIRTH_DATE, HqlConditions.IS_NOT_NULL);
 		
 		// example 1
-		filter.put(Customer.NAME, HqlConditions.LIKE_IGNORE_CASE);
+		filter.put(Customer.NAME, HqlConditions.LIKE);
 		filter.put(Customer.LAST_NAME, HqlConditions.EQUALS);
 		filter.put(Customer.DOCUMENT, HqlConditions.EQUALS);
 		
 		// example 2
 		filter.put(Customer.BIRTH_DATE_START, HqlConditions.GREATER_EQUALS);
-		filter.put(Customer.BIRTH_DATE_END, HqlConditions.LOWER_EQUALS);
+		filter.put(Customer.BIRTH_DATE_END, HqlConditions.LOWER_THAN);
 		
 		// example 3
 		filter.put(Customer.ORDERS_PRODUCTS_NAME, HqlConditions.LIKE_IGNORE_CASE);
