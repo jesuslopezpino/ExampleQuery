62f02e403244a99e31320f4739111ed40bc2dbc6
jesuslopezpino <jesus_lopez_pino@hotmail.com>
2017-12-06 18:13:01 +0100

    instance fix

--- src/main/java/foo/bar/annotations/readers/ReferenceReader.java
+++ src/main/java/foo/bar/annotations/readers/ReferenceReader.java
@@ -6,47 +6,36 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 import foo.bar.annotations.Reference;
+import foo.bar.utils.Utils;
 
 public class ReferenceReader {
 
 	private static final Logger LOG = LogManager.getLogger(ReferenceReader.class);
 
-	public static String getReferenceForField(String fieldName, Object object) {
+	public static String getReferenceForField(String fieldName, Object object)
+			throws NoSuchFieldException, SecurityException {
 		String result = null;
-		try {
-			Field field = object.getClass().getDeclaredField(fieldName);
-			final Reference reference = field.getDeclaredAnnotation(Reference.class);
-			result = reference.referenceFor();
-		} catch (NoSuchFieldException e) {
-			LOG.error(e.getMessage(), e);
-			// TODO: throw exception
-		}
-		return result;
-	}
-	
-	public static String getReferenceFieldName(String fieldName, Object object) {
-		String result = null;
-		try {
-			Field field = object.getClass().getDeclaredField(fieldName);
-			final Reference reference = field.getDeclaredAnnotation(Reference.class);
-			result = reference.fieldName();
-		} catch (NoSuchFieldException e) {
-			LOG.error(e.getMessage(), e);
-			// TODO: throw exception
-		}
+		Field field = Utils.getField(fieldName, object);
+		final Reference reference = field.getDeclaredAnnotation(Reference.class);
+		result = reference.referenceFor();
 		return result;
 	}
 
-	public static boolean isReferenceField(String fieldName, Object object) {
+	public static String getReferenceFieldName(String fieldName, Object object) throws NoSuchFieldException, SecurityException {
+		String result = null;
+		Field field = Utils.getField(fieldName, object);
+		final Reference reference = field.getDeclaredAnnotation(Reference.class);
+		result = reference.fieldName();
+		return result;
+	}
+
+	public static boolean isReferenceField(String fieldName, Object object)
+			throws NoSuchFieldException, SecurityException {
 		boolean result = false;
-		try {
-			Field field = object.getClass().getDeclaredField(fieldName);
-			final Reference reference = field.getDeclaredAnnotation(Reference.class);
-			result = reference != null;
-		} catch (NoSuchFieldException e) {
-			LOG.error(e.getMessage(), e);
-			// TODO: throw exception
-		}
+		LOG.info("isReferenceField: " + fieldName + " from class: " + object.getClass());
+		Field field = Utils.getField(fieldName, object);
+		final Reference reference = field.getDeclaredAnnotation(Reference.class);
+		result = reference != null;
 		return result;
 	}
 }
