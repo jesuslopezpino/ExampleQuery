bfe6e809d61e0943c9c9046f2424dd3108782513
jesuslopezpino <jesus_lopez_pino@hotmail.com>
2017-12-17 04:47:50 +0100

    preparing for next usage

---
+++ src/main/java/foo/bar/service/query/CustomField.java
@@ -0,0 +1,34 @@
+package foo.bar.service.query;
+
+public class CustomField {
+
+	private HqlFunctions function;
+
+	private String fieldName;
+
+	public CustomField(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public CustomField(String fieldName, HqlFunctions function) {
+		this.fieldName = fieldName;
+		this.function = function;
+	}
+
+	public String getFieldName() {
+		return this.fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public HqlFunctions getFunction() {
+		return this.function;
+	}
+
+	public void setFunction(HqlFunctions function) {
+		this.function = function;
+	}
+
+}
---
+++ src/main/java/foo/bar/service/query/CustomSelect.java
@@ -0,0 +1,47 @@
+package foo.bar.service.query;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomSelect {
+
+	private boolean distinct;
+
+	private List<CustomField> fields;
+
+	public CustomSelect() {
+		this.fields = new ArrayList<>();
+		this.distinct = false;
+	}
+
+	public CustomSelect(boolean distinct) {
+		this.fields = new ArrayList<>();
+		this.distinct = distinct;
+	}
+
+	public void addField(String field) {
+		CustomField customField = new CustomField(field);
+		this.fields.add(customField);
+	}
+
+	public void addField(CustomField customField) {
+		this.fields.add(customField);
+	}
+
+	public boolean isDistinct() {
+		return this.distinct;
+	}
+
+	public void setDistinct(boolean distinct) {
+		this.distinct = distinct;
+	}
+
+	public List<CustomField> getFields() {
+		return this.fields;
+	}
+
+	public void setFields(List<CustomField> fields) {
+		this.fields = fields;
+	}
+
+}
